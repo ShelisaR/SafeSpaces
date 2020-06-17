839a66077aef64f4cb006c9e033bb336b23812d7
nia321 <66578622+nia321@users.noreply.github.com>
2020-06-17 17:26:10 -0400

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { type } from 'os';
 
 @Component({
   selector: 'app-tab3',
@@ -10,7 +11,21 @@ export class Tab3Page {
 
   constructor() {}
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  sendForm(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    else {
+      const placename = form.value.placename;
+      const placetype = form.value.placetype;
+      const address = form.value.address;
+      const city = form.value.city;
+      const state = form.value.state;
+      const zipcode = form.value.zipcode;
+      const comments = form.value.comments;
+
+      // test to make sure the program is getting the correct information
+      console.log(placename, placetype, address, city, state, zipcode, comments);
+    }
   }
 }
