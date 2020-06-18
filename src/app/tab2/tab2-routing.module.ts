d21713ca4261b964253374795857baa78c976e83
Shelisa Ross <shelisa.ross@gmail.com>
2020-06-17 20:12:23 -0500

    Tab2

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -2,11 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab2Page } from './tab2.page';
 
+import { Tab2HomeComponent } from './tab2-home/tab2-home.component';
+import { GermsDetailComponent } from './germs-detail/germs-detail';
+import { GermsDetailProviderComponent } from '/germs-detail-provider/germs-detail-provider';
 const routes: Routes = [
-  {
-    path: '',
-    component: Tab2Page,
-  }
+    {
+        path: 'Tab2',
+        component: Tab2Page,
+        children: [
+            {
+                path: '',
+                component: Tab2Home,
+                children: [
+                    {
+                        path: '',
+                        component: GermsDetail,
+                        children: [
+                            {
+                                path: ':id',
+                                component: GermsDetailProvider
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 ];
 
 @NgModule({
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -6,6 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+import { Tab2HomeComponent } from './tab2-home/tab2-home.component';
+import { GermsDetailComponent } from './germs-detail/germs-detail';
+import { GermsDetailProviderComponent } from '/germs-detail-provider/germs-detail-provider';
+
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
@@ -16,6 +20,12 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+    declarations:
+    [
+    Tab2Page,
+    Tab2Home,
+    GermsDetail,
+    GermsDetailProvider
+    ]
 })
 export class Tab2PageModule {}
