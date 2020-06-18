799b3421f0e1df4bac038949ab91a6ae0ee3813b
nia321 <66578622+nia321@users.noreply.github.com>
2020-06-18 11:53:45 -0400

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms'; // for the suggestion form
 import { AlertController } from '@ionic/angular'; // for the confirmation of the form being sent
-// import { type } from 'os';
 
 @Component({
   selector: 'app-tab3',
