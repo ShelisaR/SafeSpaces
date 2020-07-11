85e38a038887112fccce880db7b3dc684dcfc56a
Shelisa Ross <shelisa.ross@gmail.com>
2020-07-10 20:29:03 -0500

    changes

--- src/app/tab1/login/login.component.ts
+++ src/app/tab1/login/login.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  
-    constructor(public auth: AuthService) { }
 
+    constructor(public auth: AuthService, public toastController: ToastController) { }
+
+    async logoutAlert() {
+        const toast = await this.toastController.create({
+            message: 'You have successfully logged out.',
+            duration: 2000
+        });
+        toast.present();
+    }
 }
+
